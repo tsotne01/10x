625c9dbd586b03e60645e293f05a3653daa38408
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-04-19 21:42:59 +0400

    feat: uploading progress.  ~ _ ~

--- session 26 - homework/3. Avengers Assemble (Reduce)/solution.js
+++ session 26 - homework/3. Avengers Assemble (Reduce)/solution.js
@@ -1,3 +1,10 @@
 // The Avengers need funding. Use the `reduce()` method to sum up the total budget required for each Avenger’s mission.
 // Example input: `[ {hero: 'Iron Man', budget: 5000}, {hero: 'Hulk', budget: 2000} ]`
 // Expected output: `Total budget: 7000`
+
+
+const avengers = [ {hero: 'Iron Man', budget: 5000}, {hero: 'Hulk', budget: 2000} ];
+
+const totalBudget = avengers.reduce((acc,cur)=> acc + cur.budget,0);
+
+console.log(totalBudget);
